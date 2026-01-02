46b0bf22b8f60c1a58e52a25da7ecdbd86f48d88
Arxemond777 <1arxemond1@gmail.com>
2026-01-02 22:10:45 +0400

    add terraform infra and deploy workflow

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 ### 1) GitHub Actions (CI) to build a jar and (optionally) a Docker image for every push to master.  
 Add the file .github/workflows/ci.yml  
+
+### 3) install terraform and aws cli (optional for windows)
+```bash
+winget install -e --id Amazon.AWSCLI
+winget install -e --id Hashicorp.Terraform
+
+# set up aws credentials
+aws configure
+# generate an ssh key pair for ec2 instance access
+ssh-keygen -t ed25519 -f ~/.ssh/terraform-ec2 -C "terraform-ec2"
+
+cd infra
+terraform init
+terraform apply
+```
+
+git repository -> Settings -> Secrets and variables → Actions → New repository secret  
+```bash
+EC2_HOST = ec2 ip  
+EC2_SSH_KEY = cat ~/.ssh/terraform-ec2
+```
+
+Make Docker image in GHCR and deploy via SSH
+.github/workflows/deploy.yml
---
+++ infra/main.tf
@@ -0,0 +1,73 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_ami" "al2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+resource "aws_key_pair" "this" {
+  key_name   = "terraform-ec2"
+  public_key = file(pathexpand("~/.ssh/terraform-ec2.pub"))
+}
+
+resource "aws_security_group" "web" {
+  name   = "demo-web"
+  vpc_id = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["195.56.119.209/32"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app" {
+  ami                         = data.aws_ami.al2023.id
+  instance_type               = "t3.micro"
+  subnet_id                   = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  key_name                    = aws_key_pair.this.key_name
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+    #!/bin/bash
+    set -eux
+    dnf -y update
+    dnf -y install docker
+    systemctl enable --now docker
+  EOF
+}
